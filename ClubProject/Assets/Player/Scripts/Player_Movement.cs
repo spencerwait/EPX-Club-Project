edd39e6a74438ac3b4869e9f718f61743d6ef813
Alec <acleofe@uiowa.edu>
2020-09-19 04:54:05 -0500

    Added Some Sweet Placeholder Assets
    Added some art from Studnet #XYZ and put in a basic Player_Movement Script to move the BulbMan with WASD

---
+++ ClubProject/Assets/Player/Scripts/Player_Movement.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_Movement : MonoBehaviour
+{
+
+
+    private Rigidbody2D rb;
+    public float speed = 0.0f;
+    private Vector2 moveVelocity;
+
+    // Use this for initialization
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rb.constraints = RigidbodyConstraints2D.FreezeRotation;
+        Vector2 moveInput = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        moveVelocity = moveInput.normalized * speed;
+    }
+
+    void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + moveVelocity * Time.fixedDeltaTime);
+    }
+}
