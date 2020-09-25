47cc53a20c4ecd870131cda858cd05a3f93673cc
Alec <acleofe@uiowa.edu>
2020-09-24 23:54:23 -0500

    Fixed A Bunch Of Things and added 2D Lighting

---
+++ ClubProject2/Assets/ScriptsGeneral/FollowPoint.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPoint : MonoBehaviour
+{
+    //Object that is being tracked
+    public GameObject target;
+    private Transform follower;
+
+    public Vector3 offset;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        follower = GetComponent<Transform>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        follower.position = target.GetComponent<Transform>().position + offset;
+        
+    }
+}
