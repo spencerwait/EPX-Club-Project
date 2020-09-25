792e29691f2db92b7ea4b13a6f7880836d7b95e8
Alec <acleofe@uiowa.edu>
2020-09-25 12:11:11 -0500

    Adjusted FollowMouse and added some lanterns

---
+++ ClubProject2/Assets/EnemyFollow.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyFollow : MonoBehaviour {
+
+    public float speed = 0.0f;
+    public float stoppingDistance = 0.0f;
+    private Transform target;
+
+	// Use this for initialization
+	void Start () {
+        target = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        target = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+        if (Vector2.Distance(transform.position, target.position) > stoppingDistance)
+        {
+            transform.position = Vector2.MoveTowards(transform.position, target.position, speed * Time.deltaTime);
+        }
+
+     
+
+	}
+
+    void OnTriggerEnter2D(Collider2D hitInfo)
+    {
+        Debug.Log(hitInfo.name);
+
+        if(hitInfo.name == target.name)
+        {
+            Destroy(gameObject);
+        }
+        
+    }
+
+}
--- ClubProject2/Assets/ScriptsGeneral/FollowMouse.cs
+++ ClubProject2/Assets/ScriptsGeneral/FollowMouse.cs
@@ -5,12 +5,19 @@ using UnityEngine;
 public class FollowMouse : MonoBehaviour
 {
 
-    private float temp = 1f;
+    public float speed = 0.0f;
+    public float stoppingDistance = 0.0f;
+    private Vector3 target;
 
+
+    // Update is called once per frame
     void Update()
     {
-        
-        transform.position = new Vector3(Input.mousePosition.x,Input.mousePosition.y, temp);        
-        
+        target = new Vector3(Input.mousePosition.x, Input.mousePosition.y, 0f);
+        //target = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+        if (Vector2.Distance(transform.position, target ) > stoppingDistance)
+        {
+            transform.position = Vector2.MoveTowards(transform.position, target, speed * Time.deltaTime);
+        }
     }
 }
