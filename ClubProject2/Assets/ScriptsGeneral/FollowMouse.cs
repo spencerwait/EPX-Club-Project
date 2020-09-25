81e5ffaa073f53484ce2639cf41dfb3880106c90
Alec <acleofe@uiowa.edu>
2020-09-25 01:00:33 -0500

    Testing Will-O-Wisp Mechanics

---
+++ ClubProject2/Assets/ScriptsGeneral/FollowMouse.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowMouse : MonoBehaviour
+{
+
+    private float temp = 1f;
+
+    void Update()
+    {
+        
+        transform.position = new Vector3(Input.mousePosition.x,Input.mousePosition.y, temp);        
+        
+    }
+}
