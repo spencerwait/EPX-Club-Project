56c92243d03244db85a078578175ea27607e1d5d
Alec <acleofe@uiowa.edu>
2020-09-28 20:06:54 -0500

    Got Started On Will-O-Wisps First Attk

---
+++ ClubProject2/Assets/ScriptsGeneral/WispAttacks.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WispAttacks : MonoBehaviour  
+{
+    public float radius;
+    public System.String enemyTag;
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            print("Click");
+            Collider[] hitColliders = Physics.OverlapSphere(transform.position, radius);
+            foreach (var hitCollider in hitColliders)
+            {
+                print(hitCollider);
+                if (hitCollider.tag == enemyTag){
+                    Destroy(hitCollider);
+                }
+            }
+        }
+            
+    }
+}
