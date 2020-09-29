2b58455cd64b624050a01c1b1209d641ad7037e7
Alec <acleofe@uiowa.edu>
2020-09-29 13:34:05 -0500

    First Wisp Attack Kinda Working Now

--- ClubProject2/Assets/ScriptsGeneral/WispAttacks.cs
+++ ClubProject2/Assets/ScriptsGeneral/WispAttacks.cs
@@ -6,21 +6,39 @@ public class WispAttacks : MonoBehaviour
 {
     public float radius;
     public System.String enemyTag;
+    public Animator lightAnim;
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetMouseButtonDown(0))
+        if (Input.GetMouseButtonDown(0) && !lightAnim.GetBool("isClicked"))
         {
             print("Click");
-            Collider[] hitColliders = Physics.OverlapSphere(transform.position, radius);
+            
+            Collider2D[] hitColliders = Physics2D.OverlapCircleAll(transform.position, radius);
             foreach (var hitCollider in hitColliders)
             {
                 print(hitCollider);
                 if (hitCollider.tag == enemyTag){
-                    Destroy(hitCollider);
+                    Destroy(hitCollider.gameObject);
                 }
             }
-        }
+            lightAnim.SetBool("isClicked", true);
+            Invoke("setClickFalse", 1);
             
+
+        }
+
+    }
+
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawSphere(transform.position, radius);
+    }
+    public void setClickFalse()
+    {
+        lightAnim.SetBool("isClicked", false);
+
     }
 }
---
+++ ClubProject2/Assets/TestScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestScript : MonoBehaviour
+{
+
+    public float rotateSpeed = 20f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(0f, 0f, rotateSpeed * Time.deltaTime);
+    }
+}
