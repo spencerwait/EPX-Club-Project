855c0b09305db4bf989062802b53bef3471a71c6
Alec <acleofe@uiowa.edu>
2020-10-16 18:48:03 -0500

    Not Much Progress
    Tried to get EnemyGrab to work but am having problems with the 2D Collision Detection

---
+++ ClubProject2/Assets/EnemyGrab.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyGrab : MonoBehaviour
+{
+
+    private GameObject player;
+
+    private void OnCollisionStay2D(Collision2D collision)
+    {
+        print(collision.gameObject.tag);
+        if (collision.gameObject.tag.Contains("Player"))
+        {
+            print("Player Grabbed");
+            player = collision.gameObject;
+            (player.GetComponent("Player_Moverment") as MonoBehaviour).enabled = false;
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        print(collision.gameObject.tag);
+        if (collision.gameObject.tag.Contains("Player"))
+        {
+            print("Player Grabbed");
+            player = collision.gameObject;
+            (player.GetComponent("Player_Moverment") as MonoBehaviour).enabled = false;
+        }
+    }
+}
